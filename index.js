d3bc8968597637e5705a85b9558d1e43577eb578
DinhNguyen <nguyendinhdinh16112000@gmail.com>
2022-05-30 17:52:07 +0700

    add folder build

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ import connect from './src/database/connect.js'
 import route from './src/route.js'
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 connect();
 
 app.use(cors())
--- src/route.js
+++ src/route.js
@@ -3,9 +3,9 @@ import {index, api_list_item} from './Controller.js'
 
 function route(app){
     
-    app.get('/', (req, res)=>{
-        index(req, res)
-    })
+    // app.get('/', (req, res)=>{
+    //     index(req, res)
+    // })
 
     app.get('/api_shop', (req, res)=>{
         api_list_item(req, res)
