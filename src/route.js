ae858d6dae11a836386451518aef780c2f369738
DinhNguyen <nguyendinhdinh16112000@gmail.com>
2022-06-15 21:55:33 +0700

    modifile route

--- src/route.js
+++ src/route.js
@@ -4,19 +4,19 @@ import {index, api_list_item, login, register} from './Controller.js'
 
 function route(app){
     
-    app.get('/', (req, res)=>{
+    app.get('https://git.heroku.com/server-react-s.git/', (req, res)=>{
         index(req, res)
     })
 
-    app.get('/api/shop/All', (req, res)=>{
+    app.get('https://git.heroku.com/server-react-s.git/api/shop/All', (req, res)=>{
         api_list_item(req, res)
     })
 
-    app.post('/login', (req, res)=>{
+    app.post('https://git.heroku.com/server-react-s.git/login', (req, res)=>{
         login(req, res)
     })
 
-    app.post('/register', (req, res)=>{
+    app.post('https://git.heroku.com/server-react-s.git/register', (req, res)=>{
         register(req, res)
     })
 }
